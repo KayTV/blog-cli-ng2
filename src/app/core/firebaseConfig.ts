f542ca20965574f79013777a5db97d03075a9bf7
Kaylyn Van Norstrand <kaylyn.vannorstrand@gmail.com>
2016-12-08 18:11:31 -0500

    added in firebase to angular2

---
+++ src/app/core/firebaseConfig.ts
@@ -0,0 +1,7 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyA_1071SdBKPKjXXdLSoFhgD8yEBnR8Qhg",
+  authDomain: "dummy-use-data.firebaseapp.com",
+  databaseURL: "https://dummy-use-data.firebaseio.com",
+  storageBucket: "dummy-use-data.appspot.com",
+  messagingSenderId: "1061835085089"
+};
