d0f2d8e1f798204457688e3550538483d855dedc
Kaylyn Van Norstrand <kaylyn.vannorstrand@gmail.com>
2016-12-13 18:02:39 -0500

    model for post added

--- src/app/components/homepage/homepage.component.ts
+++ src/app/components/homepage/homepage.component.ts
@@ -1,16 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 import { AngularFire, FirebaseListObservable } from 'angularfire2';
 
+import { Post } from '../../models/posts';
+
 @Component({
   selector: 'homepage',
   templateUrl: './homepage.html',
   styleUrls: ['../../styles/homepage.css', '../../styles/shared.css']
 })
 
-export class HomepageComponent {
+export class HomepageComponent implements OnInit {
   title = 'app works!';
-  items: FirebaseListObservable<any[]>;
-  constructor(af: AngularFire) {
-    this.items = af.database.list('/posts');
+  items: FirebaseListObservable<Post[]>;
+  constructor(private af: AngularFire) { }
+
+  ngOnInit() {
+    this.items = this.af.database.list('/posts');
   }
 }
---
+++ src/app/models/posts.ts
@@ -0,0 +1,7 @@
+export class Post {
+  id: number;
+  description: string;
+  post: string;
+  title: string;
+  $key: string;
+}
