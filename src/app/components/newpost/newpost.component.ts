a2d82bda14577973be911aaf20c731447bd80523
Kaylyn Van Norstrand <kaylyn.vannorstrand@gmail.com>
2016-12-13 18:17:01 -0500

    basic post request works

--- src/app/components/newpost/newpost.component.ts
+++ src/app/components/newpost/newpost.component.ts
@@ -1,4 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AngularFire, FirebaseListObservable } from 'angularfire2';
+import { ActivatedRoute, Params } from '@angular/router';
+import { Location } from '@angular/common';
+
+import { Post } from '../../models/posts';
 
 @Component({
   selector: 'new-post',
@@ -6,4 +11,20 @@ import { Component } from '@angular/core';
   styleUrls: ['../../styles/shared.css']
 })
 
-export class NewPostComponent { }
+export class NewPostComponent implements OnInit {
+  items: FirebaseListObservable<Post[]>;
+
+  constructor(
+    private af: AngularFire,
+    private location: Location
+  ) { }
+
+  ngOnInit() {
+    this.items = this.af.database.list('/posts');
+  }
+
+  addPost(title: string, body: string, description: string) {
+    this.items.push({title: title, post: body, description: description})
+    this.location.back()
+  }
+}
